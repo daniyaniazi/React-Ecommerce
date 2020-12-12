0d191ef38b2b98e4003644f702bf4fb74a6ed909
daniyaniazi <farihadania@hotmail.com>
2020-12-13 00:18:47 +0500

    style background image

--- src/components/menu-items/menu-items.component.js
+++ src/components/menu-items/menu-items.component.js
@@ -2,11 +2,13 @@ import React from "react";
 import './menu-items.styles.scss'
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`,
-    }} className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+        <div
+            className=' backgroundImage ' style={{
+                backgroundImage: `url(${imageUrl})`,
+            }} />
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className='subtitle'>Shop now</span>
         </div>
     </div>
