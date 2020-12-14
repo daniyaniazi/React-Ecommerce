aab59cd054dc9e2ca752599d3cd44c3f6ef8ebdc
daniyaniazi <farihadania@hotmail.com>
2020-12-14 15:30:24 +0500

    Preview of Category component

---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './collection-preview.styles.scss'
+import CollectionItem from "../collection-item/collection-item.component";
+
+const CollectionPreview = ({ title, items }) => (
+    <div className='collection-preview'>
+        <h1 className='title'>
+            {title.toUpperCase()}
+        </h1>
+        <div className='preview'>
+            {
+                items.filter((item, idx) => idx < 4).
+                    map(({ id, ...itemProps }) => (
+                        <CollectionItem key={id} {...itemProps} />
+                    ))}
+        </div>
+    </div>
+
+)
+export default CollectionPreview;
