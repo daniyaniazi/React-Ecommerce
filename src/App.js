c7fedb1aa83cd0a3af06615d69f010bfd7c2db56
daniyaniazi <farihadania@hotmail.com>
2020-12-14 15:31:08 +0500

    update app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from 'react-router-dom';
 
-const HatsPage = () => (
-  <div> <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
