89d2a4475f6c32307f3fe05f187e41dbc7e85366
daniyaniazi <farihadania@hotmail.com>
2020-12-19 23:24:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from 'react-router-dom';
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signIn' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
