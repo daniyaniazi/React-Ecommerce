ff93620eeddd3609d35a7d1912e8c4ce32d54828
daniyaniazi <farihadania@hotmail.com>
2020-12-19 23:35:55 +0500

    Added a custom button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component'
+import CustomButton from "../custom-button/custom-button.component";
 class SignIn extends React.Component {
 
     constructor(props) {
@@ -28,7 +29,7 @@ class SignIn extends React.Component {
         return (
             <div className='sign-in'>
                 <h2>
-                    I alreasy have an account
+                    I already have an account
 </h2><span>
                     sign in with your email and password
 </span>
@@ -40,7 +41,7 @@ class SignIn extends React.Component {
                     <FormInput value={this.state.password}
                         handleChange={this.handleChange} type="password" name="password" id="password"
                         label='password' />
-                    <input type="submit" value='Submit Form' />
+                    <CustomButton type="submit"> Sign In</CustomButton>
                 </form>
 
             </div>
