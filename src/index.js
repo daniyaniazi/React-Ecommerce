725fb083c7a668b2a8f1e9e5eae12e544f4755e7
daniyaniazi <farihadania@hotmail.com>
2021-01-17 21:30:08 +0500

    reducer intro

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom' //gives routing fucntionality
+import { Provider } from 'react-redux'
+
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider >,
   document.getElementById('root')
 );
 
