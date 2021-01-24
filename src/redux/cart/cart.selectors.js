678e69d6b38660b8bd0792ce6617f1769ff68b62
daniyaniazi <farihadania@hotmail.com>
2021-01-25 01:09:57 +0500

    added more selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -12,3 +12,8 @@ export const selectCartItemsCount = createSelector(
     [selelctCartItems],
     cartItems => cartItems.reduce((acumulatedQuantity, cartItem) => acumulatedQuantity + cartItem.quantity, 0)
 )
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+);
