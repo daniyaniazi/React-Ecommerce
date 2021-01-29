cf37b5cf4ed4f913ef61390fd6964cfc48792a00
daniyaniazi <farihadania@hotmail.com>
2021-01-29 16:30:57 +0500

    added local storage for cart using redux persist

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom' //gives routing fucntionality
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { store, persistor } from './redux/store'
+import { PersistGate } from "redux-persist/integration/react";
 
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+
     </BrowserRouter>
   </Provider >,
   document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger'
 import rootReducer from "./root-reducer";
-
+import { persistStore } from 'redux-persist'
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
-
-export default store;
+const persistor = persistStore(store)
+export { persistor, store };
 
